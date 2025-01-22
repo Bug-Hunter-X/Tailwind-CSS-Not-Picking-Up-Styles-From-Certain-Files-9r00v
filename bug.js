e58d54d7b69eec2a5f555681599a54ea2a2f59bb
BugHunter-X <askzenai@gmail.com>
2025-01-22 09:44:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// ... your Tailwind CSS configuration ...
+module.exports = {
+  // ... other configurations ...
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'], // Missing file extensions
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+```
